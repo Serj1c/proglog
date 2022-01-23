1040cad2400274ef5bdcc8dfce9a33a4e95c3312
Serj1c <sergej.a.isaev@gmail.com>
2022-01-23 13:27:29 +0300

    add segment tests

---
+++ internal/log/segment_test.go
@@ -0,0 +1,56 @@
+package log
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	api "github.com/serj1c/proglog/api/v1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSegment(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "segment-test")
+	defer os.RemoveAll(dir)
+
+	want := &api.Record{Value: []byte("hello world")}
+
+	c := Config{}
+	c.Segment.MaxStoreBytes = 1024
+	c.Segment.MaxIndexBytes = entryWidth * 3
+
+	s, err := NewSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.Equal(t, uint64(16), s.nextOffset)
+	require.False(t, s.IsMaxed())
+
+	for i := uint64(0); i < 3; i++ {
+		off, err := s.Append(want)
+		require.NoError(t, err)
+		require.Equal(t, 16+i, off)
+
+		got, err := s.Read(off)
+		require.NoError(t, err)
+		require.Equal(t, want.Value, got.Value)
+	}
+
+	_, err = s.Append(want)
+	require.Equal(t, io.EOF, err)
+
+	require.True(t, s.IsMaxed())
+
+	c.Segment.MaxStoreBytes = uint64(len(want.Value) * 3)
+	c.Segment.MaxIndexBytes = 1024
+
+	s, err = NewSegment(dir, 16, c)
+	require.NoError(t, err)
+
+	require.True(t, s.IsMaxed())
+
+	err = s.Remove()
+	require.NoError(t, err)
+	s, err = NewSegment(dir, 16, c)
+	require.NoError(t, err)
+	require.False(t, s.IsMaxed())
+}
